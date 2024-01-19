d4df886648e17aeaa546536e77c100760e41eafc
ak_alam <akbar.alam@systemsltd.com>
2024-01-19 17:33:12 +0500

    feat!: EKS Module (EKS Cluster, Cluster IAM Roles, OIDC, NodeGroup, NodeGroup IAM roles)

--- provider.tf
+++ provider.tf
@@ -1,22 +1,3 @@
-# terraform {
-# #   required_version = ">= 1.5.4" #terraform version
-#   #provider name and version
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "5.15.0"
-#     }
-#   }
-# }
-
-# #provider configuration
-# provider "aws" {
-#   # Configuration options
-#   region  = "us-east-1"
-#   profile = "default"
-# }
-
-
 terraform {
   required_providers {
     aws = {
